1ec013987fb3b0bd403aa035efacf5626d00648b
Gabriel dos Santos Souto <115189057+gabesouto@users.noreply.github.com>
2024-07-10 16:03:36 -0300

    Update Header.tsx

--- app/frontend/src/pages/products/components/Header.tsx
+++ app/frontend/src/pages/products/components/Header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
             </li>
             <li>
               <a
-                href="https://github.com/gabesouto/lexart-test-tecnico"
+                href="https://github.com/gabesouto/my-storage"
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Repository
